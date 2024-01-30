5e3c0ae756f05203083d403100e0eb37befa0b91
tonyfernandes991 <112737052+tonyfernandes991@users.noreply.github.com>
2024-01-30 16:56:26 -0300

    Api

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const TagsController = require("../controllers/TagsControllers");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const tagsRoutes = Router();
+const tagsController = new TagsController();
+
+tagsRoutes.get("/", ensureAuthenticated, tagsController.index);
+
+module.exports = tagsRoutes;
